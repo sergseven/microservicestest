50506f22b928b564f813b5e0fd129eff26079bb2
serg.seven <serg.seven@gmail.com>
2019-01-09 22:35:24 +0100

    Add contract for get account number API

---
+++ springbooteurekademo/web-server-reactive/src/test/resources/contract/demo/reactive/get_account_number.groovy
@@ -0,0 +1,21 @@
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    description "should return any non-empty account details"
+
+    request {
+        url "/account"
+        method GET()
+    }
+
+    response {
+        status OK()
+        headers {
+            contentType applicationJson()
+        }
+        body(
+                accountNumber: "12345",
+                owner: "foo",
+        )
+    }
+}
