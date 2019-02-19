275a416013a0127e5e1dd2981060880017638b7b
serg.seven <serg.seven@gmail.com>
2019-02-19 12:53:17 +0100

    Make AccountsController perform a call to remote API

--- springbooteurekademo/server1/src/main/java/com/svn/test/spring/boot/eureka/demo/server/account/AccountsConfiguration.java
+++ springbooteurekademo/server1/src/main/java/com/svn/test/spring/boot/eureka/demo/server/account/AccountsConfiguration.java
@@ -1,7 +1,9 @@
 package com.svn.test.spring.boot.eureka.demo.server.account;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * The accounts Spring configuration.
@@ -9,4 +11,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan
 public class AccountsConfiguration {
+
+  @Bean
+  RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
 }
--- springbooteurekademo/server1/src/main/java/com/svn/test/spring/boot/eureka/demo/server/account/AccountsController.java
+++ springbooteurekademo/server1/src/main/java/com/svn/test/spring/boot/eureka/demo/server/account/AccountsController.java
@@ -1,26 +1,42 @@
 package com.svn.test.spring.boot.eureka.demo.server.account;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.svn.test.spring.boot.eureka.demo.server.account.dto.AccountDto;
 import com.svn.test.spring.boot.eureka.demo.server.account.entity.Account;
+import com.svn.test.spring.boot.eureka.demo.server.account.mapper.AccountMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.logging.Logger;
-
+@Slf4j
 @RestController
 public class AccountsController {
 
-  private final Logger logger = Logger.getLogger(AccountsController.class
-          .getName());
+  @Autowired
+  RestTemplate restTemplate;
+
+  @Autowired
+  AccountMapper accountMapper;
 
   /**
    * Fetch an account with the specified account number.
    */
   @RequestMapping("/accounts/{accountNumber}")
   public Account byNumber(@PathVariable("accountNumber") String accountNumber) {
-    logger.info("accounts-service byNumber() invoked: " + accountNumber);
-    Account account = Account.builder().number(accountNumber).build();
-    logger.info("accounts-service byNumber() found: " + account);
+    log.info("accounts-service byNumber() invoked: " + accountNumber);
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    String accountDtoBody = restTemplate.getForObject("http://localhost:8080/account", String.class);
+    log.info("got response from remote accountDtoBody: " + accountDtoBody);
+
+    AccountDto accountDto = restTemplate.getForObject("http://localhost:8080/account", AccountDto.class);
+    log.info("got response from remote AccountDto: " + accountDto);
+    Account account = accountMapper.from(accountDto);
+    log.info("accounts-service byNumber() found: " + account);
     return account;
   }
 
